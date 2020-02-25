2b0007f193375d6308be490b23314886bd18fb9e
XueKai <kkrabbit1@gmail.com>
2020-02-25 10:11:08 +0800

    Initial Commit

---
+++ GitHubTestRes/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GitHubTestRes
+//
+//  Created by xx on 2020/2/25.
+//  Copyright © 2020 Tsinghua University. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
